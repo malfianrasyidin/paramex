7b51ee04bd6aedfe19a341b2dda166bfa86bad83
Muhammad Alfian Rasyidin <malfianrasyidin@gmail.com>
2021-01-29 17:09:15 +0700

    updade

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import { Button } from "components";
+import { Button } from 'components';
 
 const App = () => (
   <div className="d-flex justify-content-center mt-4">
     <Button label="Button" />
   </div>
-)
+);
 
-export default App
+export default App;
--- src/components/__tests__/button.tsx
+++ src/components/__tests__/button.tsx
@@ -1,12 +1,9 @@
 import { render } from '@testing-library/react';
 import { Button } from 'components';
 
-
 describe('Button', () => {
   it('renders correctly', () => {
-    const { container } = render(
-      <Button label="example" outline block />
-    );
+    const { container } = render(<Button label="example" outline block />);
 
     expect(container).toMatchSnapshot();
   });
